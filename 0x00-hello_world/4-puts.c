cbfc2fe58b3dac5f257706cd7ba035fd79aa87b1
NAsarehene <princeasarehene97@gmail.com>
2023-07-15 21:41:56 -0700

    4-puts.cclear

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,6 @@
  * main - Display Programming is like building a multilingual puzzle
  * Return: 0(complete succefully)
  */
-
 int main(void)
 {
 	put("\"Programming is like building a multilingual puzzle");
