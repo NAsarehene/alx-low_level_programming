d3e457439df99a1dea7befc99ab65df85f93ecad
NAsarehene <princeasarehene97@gmail.com>
2023-07-15 22:54:18 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A programmer that print exactly with proper grammar, but the outcome is a piece of art,
+ * Return: o (sucessful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
